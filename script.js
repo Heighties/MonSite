7621f56d400c0360900d6c7b49e07279cc0a642c
Heighties <heighties@gmail.com>
2021-09-05 12:21:53 +0200

    smooth scrolling "rajoutée" (toujours problème JS)

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ $(document).ready(function(){
         $('header').removeClass('toggle');
 
     });
+
+    //smooth scrolling
+
+    $('a[href*="#"]').on('click',function(e){
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+        500,
+        'linear'
+        
+        );
+
+
+    });
+
 });
