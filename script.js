f30b485b5ee6fd997e6d2a6192714556828d8bf6
Heighties <heighties@gmail.com>
2021-09-12 01:08:55 +0200

    ajout reveal + typewriter

--- script.js
+++ script.js
@@ -44,6 +44,10 @@ $(document).ready(function(){
 
 }); 
 
+
+//reveal
+
+
 const ratio = .1
 const options = {
     root: null,
@@ -64,3 +68,38 @@ const observer = new IntersectionObserver(handleIntersect, options)
 document.querySelectorAll('[class*="reveal-"]').forEach(function(r){
     observer.observe(r)
 })
+
+
+//effet dans about me
+
+
+var txtAnim = document.querySelector('h4');
+
+new Typewriter(txtAnim, {
+    deleteSpeed: 50,
+    loop : true
+})
+.changeDelay(200)
+.typeString('Je suis')
+.pauseFor(300)
+.changeDelay(100)
+.typeString('<span style="color :#00bfff">\ ambitieux</span>')
+.pauseFor(1000)
+.deleteChars(9)
+.typeString('<span style="color :#00bfff"> \ créatif</span>')
+.pauseFor(1000)
+.deleteChars(7)
+.typeString('<span style="color :#00bfff">\ passionné</span>')
+.pauseFor(5000)
+.start()
+
+var txtAnim = document.querySelector('h1');
+
+new Typewriter(txtAnim, {
+    deleteSpeed: 50,
+})
+.changeDelay(100)
+.typeString('<span style="color : #eee">Je m\'appelle</span> Geoffrey')
+.start()
+
+
