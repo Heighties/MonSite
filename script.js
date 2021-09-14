5a44953dc100ad17f9292613a372902e9b348e7c
Heighties <heighties@gmail.com>
2021-09-14 21:43:19 +0200

    +light mode (voir couleurs + .home h1) +style color

--- script.js
+++ script.js
@@ -107,4 +107,47 @@ new Typewriter(txtAnim, {
 
 
 
+//toggle style switcher
+
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", () =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+
+//theme colors
+
+const alternateStyles = document.querySelectorAll('.alternate-style');
+
+function setActiveStyle(color){
+    alternateStyles.forEach((style) =>{
+        if(color === style.getAttribute('title')){
+            style.removeAttribute('disabled');
+        }
+        else{
+            style.setAttribute('disabled', 'true');
+        }
+    })
+}
+
+//theme light and dark mode
+
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () =>{
+    dayNight.querySelector("i").classList.toggle("fa-moon-o");
+    dayNight.querySelector("i").classList.toggle("fa-sun-o");
+    document.body.classList.toggle("light");
+})
+
+window.addEventListener("load", () =>{
+    if(document.body.classList.contains("light")){
+        dayNight.querySelector("i").classList.add("fa-moon-o");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-sun-o");
+    }
+
+    
+})
 
